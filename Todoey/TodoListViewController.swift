3f8820a70b28633ffa1efe518dfc6ba46de01f94
gopalakrishna <gopalakrishna@gopalakrishnas-MacBook-Pro.local>
2019-03-02 18:06:35 +0530

    Add new today list functionality complete

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemarray = ["Find Milk","Buy Eggs","Destroy Demogorgan"]
+    var itemarray = ["Find Milk","Buy Eggs","Destroy Demogorgan"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,5 +41,27 @@ class TodoListViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    
+    @IBAction func barBtnPressed(_ sender: UIBarButtonItem) {
+        
+        var txtFld = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemarray.append(txtFld.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTxtFld) in
+            txtFld = alertTxtFld
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
 }
 
